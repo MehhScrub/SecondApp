00a5d87f19fd7c9f4484449ece7985bb6b6c60a4
MehhScrub <38139122+MehhScrub@users.noreply.github.com>
2019-04-10 17:30:31 +0300

    updated v2

--- app/src/main/java/com/example/secondapp/Sarasas/PersonNew.java
+++ app/src/main/java/com/example/secondapp/Sarasas/PersonNew.java
@@ -24,7 +24,7 @@ public class PersonNew extends SarasasFragment {
     public EditText mLastName;
 
     @BindView(R.id.btnAdd)
-            //lmao
+            //lmao wwwws
     Button btnAdd;
 
     private static final String TAG = "PersonNew";
