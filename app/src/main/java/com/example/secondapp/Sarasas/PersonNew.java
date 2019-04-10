265b87627d20bc657cbac1e249cc73e92142a32e
MehhScrub <38139122+MehhScrub@users.noreply.github.com>
2019-04-10 17:23:55 +0300

    waz

--- app/src/main/java/com/example/secondapp/Sarasas/PersonNew.java
+++ app/src/main/java/com/example/secondapp/Sarasas/PersonNew.java
@@ -24,6 +24,7 @@ public class PersonNew extends SarasasFragment {
     public EditText mLastName;
 
     @BindView(R.id.btnAdd)
+           // lmao
     Button btnAdd;
 
     private static final String TAG = "PersonNew";
